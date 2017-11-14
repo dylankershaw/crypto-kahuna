ee5ac352b1064247c7e25ba21ef7e69c202a289d
github username <dylankershaw@me.com>
2017-11-14 16:01:19 -0500

    cors origin bug fix

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,8 @@ module CryptoKahunaApiNew
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://crypto-kahuna.herokuapp.com'
-        resource 'https://crypto-kahuna.herokuapp.com', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
 
