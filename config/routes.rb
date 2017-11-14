9984158dc8ad1e76708581d763d4d99e2e614624
github username <dylankershaw@me.com>
2017-11-14 15:04:02 -0500

    removed create functionality for User

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create, :update]
+      resources :users, only: [:index, :show, :update]
       resources :portfolios, only: [:index, :show, :create, :update]
       resources :holdings, only: [:index, :show, :create, :update]
     end
