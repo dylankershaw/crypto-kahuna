e3c2e69e037f50be5c1b325c621cb5be93464d29
github username <dylankershaw@me.com>
2017-11-16 13:29:16 -0500

    added cryptokey to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :cash
+  attributes :id, :name, :cash, :cryptokey
   has_many :portfolios, serializer: UserPortfoliosSerializer
   # has_many :holdings, through: :portfolios
 end
